5a6aac3608e2598470d7953dac72a06fe236c50b
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2021-12-02 15:20:31 +0100

    other cases

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-const path = require('path')
-const visit = require('unist-util-visit')
-const sizeOf = require('image-size')
+const path = require("path");
+const visit = require("unist-util-visit");
+const sizeOf = require("image-size");
+const probe = require("probe-image-size");
 
-module.exports = setImageSize
+module.exports = setImageSize;
 
 function setImageSize(options) {
-  const opts = options || {}
-  const dir = opts.dir
-  return transformer
+  const opts = options || {};
+  const dir = opts.dir;
+  return transformer;
 
   function transformer(tree, file) {
-    visit(tree, 'element', visitor)
+    visit(tree, "element", visitor);
     function visitor(node) {
-      if (node.tagName === 'img') {
-        let src = node.properties.src
-        if (src.startsWith('http')) {
-            return
+      if (node.tagName === "img") {
+        let src = node.properties.src;
+        if (src.startsWith("http")) {
+          const { width, height } = await probe(src);
+          node.properties.width = width;
+          node.properties.height = height;
+          return;
         }
-        if (dir && src.startsWith('/')) {
-            src = path.join(dir, src)
+        if (dir && src.startsWith("data:image")) {
+          const { width, height } = await getImageDimensions(src);
+          node.properties.width = width;
+          node.properties.height = height;
+          return;
         }
-        const dimensions = sizeOf(src)
-        node.properties.width = dimensions.width
-        node.properties.height = dimensions.height
+        if (dir && src.startsWith("/")) {
+          src = path.join(dir, src);
+        }
+        const dimensions = sizeOf(src);
+        node.properties.width = dimensions.width;
+        node.properties.height = dimensions.height;
       }
     }
   }
+}
+
+function getImageDimensions(file) {
+  return new Promise(function (resolved, rejected) {
+    var i = new Image();
+    i.onload = function () {
+      resolved({ width: i.width, height: i.height });
+    };
+    i.src = file;
+  });
 }
